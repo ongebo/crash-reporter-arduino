b31f7c93cd3fa3e9a4af190f64ad3848c1b7d8d4
ongebo <isaacongebo@gmail.com>
2019-09-13 10:23:12 +0300

    Detect from accelerometer if crash has occurred

--- crash_reporter.ino
+++ crash_reporter.ino
@@ -1,6 +1,21 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 
+// Accelerometer pins
+const int xInput = A3;
+const int yInput = A2;
+const int zInput = A1;
+
+// minimum and maximum raw values for each accelerometer axis
+int minRawInput = 0;
+int maxRawInput = 1023;
+
+// number of samples per accelerometer axis
+const int sampleSize = 10;
+
+int previousAccel = 0;
+int currentAccel;
+
 // GPS coordinates
 double gpsLatitude;
 double gpsLongitude;
@@ -41,4 +56,61 @@ void updateGPSLocation()
     Serial.println(gpsLatitude);
     Serial.println("Longitude:");
     Serial.println(gpsLongitude);
+}
+
+int crashHasOccured()
+{
+    // read raw acceleration values
+    int xRaw = getAxisAcceleration(xInput);
+    int yRaw = getAxisAcceleration(yInput);
+    int zRaw = getAxisAcceleration(zInput);
+
+    // convert raw values to milli-g
+    long xScaled = map(xRaw, minRawInput, maxRawInput, -3000, 3000);
+    long yScaled = map(yRaw, minRawInput, maxRawInput, -3000, 3000);
+    long zScaled = map(zRaw, minRawInput, maxRawInput, -3000, 3000);
+
+    // re-scale to g
+    float xAccel = xScaled / 1000.0;
+    float yAccel = yScaled / 1000.0;
+    float zAccel = zScaled / 1000.0;
+
+    float totalAccel = sqrt(xAccel * xAccel + yAccel * yAccel + zAccel * zAccel);
+    if (previousAccel == 0)
+    {
+        previousAccel = totalAccel;
+        return 0;
+    }
+    Serial.println(totalAccel);
+    currentAccel = totalAccel;
+    int diff = currentAccel - previousAccel;
+    previousAccel = currentAccel;
+    if (abs(diff) > 0.7)
+    {
+        Serial.println("Crash detected...");
+        Serial.println("Previous Acceleration:");
+        Serial.println(previousAccel);
+        Serial.println("Current Acceleration:");
+        Serial.println(currentAccel);
+        Serial.println("Acceleration Difference:");
+        Serial.println(diff);
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+// take samples and return average
+int getAxisAcceleration(int axisPin)
+{
+    long reading = 0;
+    analogRead(axisPin);
+    delay(1);
+    for (int i = 0; i < sampleSize; i++)
+    {
+        reading += analogRead(axisPin);
+    }
+    return reading / sampleSize;
 }
