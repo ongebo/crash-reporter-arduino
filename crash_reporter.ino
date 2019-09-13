d82fa4c81b7b100cefdc84c754c714be75df6462
ongebo <isaacongebo@gmail.com>
2019-09-13 10:17:49 +0300

    Update gps coordinates

--- crash_reporter.ino
+++ crash_reporter.ino
@@ -1,10 +1,44 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+// GPS coordinates
+double gpsLatitude;
+double gpsLongitude;
+
+SoftwareSerial gpsPort(10, 11);
+TinyGPSPlus gps;
 
 void setup()
 {
-    // put your setup code here, to run once:
+    Serial.begin(9600);
+    gpsPort.begin(9600);
+    Serial.println("GPS Started...");
+    delay(5000);
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
+    updateGPSLocation();
+}
+
+void updateGPSLocation()
+{
+    while (gpsPort.available())
+    {
+        gps.encode(gpsPort.read());
+    }
+    if (gps.location.isValid())
+    {
+        gpsLatitude = gps.location.lat();
+        gpsLongitude = gps.location.lng();
+    }
+    else
+    {
+        gpsLatitude = 0.3431551;
+        gpsLongitude = 32.5914989;
+    }
+    Serial.println("Latitude:");
+    Serial.println(gpsLatitude);
+    Serial.println("Longitude:");
+    Serial.println(gpsLongitude);
 }
